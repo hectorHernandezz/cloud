3ed83e8d873f13769ad4847344c16b7ad1f536a1
hectorhernandezz <david22serrano@outlook.com>
2017-07-05 19:24:47 +0100

    si

---
+++ laravel/app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = "user";
+
+    protected $primaryKey = 'userid';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'name', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public $timestamps = false;
+}
